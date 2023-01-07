ceebf6a4855b11901ea403ef3195004933114911
yaman-shahbander <yaman.job.work@gmail.com>
2023-01-07 18:55:03 +0200

    feature/IS-refactor-code-1-branch: new seeder for railter with product

--- tests/Feature/TrackCommandTest.php
+++ tests/Feature/TrackCommandTest.php
@@ -5,6 +5,7 @@ namespace Tests\Feature;
 use App\Models\Product;
 use App\Models\Retailer;
 use App\Models\Stock;
+use Database\Seeders\RetailerWithProductSeeder;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Support\Facades\Http;
 use Tests\TestCase;
@@ -12,16 +13,12 @@ use Tests\TestCase;
 class TrackCommandTest extends TestCase
 {
     protected Product $product;
-    protected Retailer $retailer;
-    protected Stock $stock;
     protected Http $http;
 
     public function setUp(): void
     {
         parent::setUp();
         $this->product = app(Product::class);
-        $this->retailer = app(Retailer::class);
-        $this->stock = app(Stock::class);
         $this->http = app(Http::class);
     }
 
@@ -30,23 +27,10 @@ class TrackCommandTest extends TestCase
     /** @test */
     public function it_tracks_product_stock()
     {
-        $switch = $this->product->create(['name' => 'Nintendo Switch']);
-        $bestBuy = $this->retailer->create(['name' => 'Best Buy']);
-        $this->assertFalse($switch->inStock());
-        $stock = $this->stock;
-        $stock->price = 1000;
-        $stock->url = 'http://foo.com';
-        $stock->sku = '12345';
-        $stock->in_stock = false;
-        $bestBuy->addStock($switch, $stock);
-        $this->assertFalse($stock->fresh()->in_stock);
-        $this->http->fake(function () {
-            return [
-                'available' => true,
-                'price' => 29900
-            ];
-        });
+        $this->seed(RetailerWithProductSeeder::class);
+        $this->assertFalse($this->product->first()->inStock());
+        $this->http->fake(fn () => ['available' => true, 'price' => 29900]);
         $this->artisan('track');
-        $this->assertTrue($stock->fresh()->in_stock);
+        $this->assertTrue($this->product->first()->inStock());
     }
 }
